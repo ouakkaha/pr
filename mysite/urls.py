48ffdca0b544aee54dba1d01ca44d51c518fdcd6
ouakkaha <51659310+ouakkaha@users.noreply.github.com>
2019-07-04 22:09:34 +0200

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
-from django.conf.urls import include, url
+from main.views import about, blog
+
 
 urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('admin/', admin.site.urls),
-    path('', 'about/', include('main.urls')),
+    path('about/', about, name='about'),
+    path('blog/', blog, name='blog'),
 ]
