952d1fd9b100344827284e923e07fc0c2536cc9c
ouakkaha <51659310+ouakkaha@users.noreply.github.com>
2019-07-04 22:10:20 +0200

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
 from . import views
-from main import views as main_views
-from django.contrib.auth import views as auth_views
-from main.views import blog, about
-from django.conf.urls import include, url
 
-urlpatterns = [
-    path('about/', about, name='about'),
-    path('', blog, name='blog'),
-]
+urlpatterns = []
+
